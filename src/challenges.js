a2c39542b1f2113cca62482ecf113c268bd7bddc
JuniorMira <j-unyor14@hotmail.com>
2021-12-05 16:16:43 -0300

    desafio 8 ok

--- src/challenges.js
+++ src/challenges.js
@@ -4,11 +4,11 @@ function compareTrue(n1, n2) {
     return true;
   } else {
     return false;
- }
+  }
 }
 // Desafio 2
 function calcArea(base, altura) {
-  let area = (base * altura) / 2
+  let area = (base * altura) / 2;
   return area;
 }
 
@@ -32,31 +32,31 @@ console.log(footballPoints(2, 1));
 
 // Desafio 6
 function highestCount(numbers) {
-    let num = numbers[0];
-    for (let i = 0; i < numbers.length; i += 1) {
-      if (num < numbers[i]) {
-        num = numbers[i];
-      } else {
-        num = num;
-      }
+  let num = numbers[0];
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (num < numbers[i]) {
+      num = numbers[i];
+    } else {
+      num = num;
     }
-    let rep = 0;
-    for (let i = 0; i < numbers.length; i += 1) {
-      if (num === numbers[i]) {
-        rep += 1;
-      }
-    }
-    return rep;
   }
-console.log(highestCount([0,0,0]));
+  let rep = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (num === numbers[i]) {
+      rep += 1;
+    }
+  }
+  return rep;
+}
+console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let position1 = Math.abs(mouse - cat1)
-  let position2 = Math.abs(mouse - cat2)
-  if(position1 === position2){
+  let position1 = Math.abs(mouse - cat1);
+  let position2 = Math.abs(mouse - cat2);
+  if (position1 === position2) {
     return "os gatos trombam e o rato foge"
-  } else if (position1 < position2){
+  } else if (position1 < position2) {
     return 'cat1'
   } else {
     return 'cat2'
@@ -65,10 +65,21 @@ function catAndMouse(mouse, cat1, cat2) {
 console.log(catAndMouse(1, 0, 2));
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arrayNumbers) {
+  let result = [];
+  for(let i = 0; i < arrayNumbers.length; i +=1){
+    if(arrayNumbers[i] % 3 === 0 && arrayNumbers[i] % 5 === 0){
+      result.push('fizzBuzz')
+    } else if (arrayNumbers[i] % 3 === 0){
+      result.push('fizz')
+    } else if (arrayNumbers[i] % 5 === 0){
+      result.push('buzz')
+    } else {
+      result.push('bug!')
+    }
+  }return result;
 }
-
+console.log(fizzBuzz([2, 15, 7, 9, 45]))
 // Desafio 9
 function encode() {
   // seu código aqui
