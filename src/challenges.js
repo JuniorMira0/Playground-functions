3a878bb373b02d38d772c054d58845ee2840a488
JuniorMira <j-unyor14@hotmail.com>
2021-12-05 17:39:09 -0300

    Desafio 9 ok

--- src/challenges.js
+++ src/challenges.js
@@ -2,9 +2,8 @@
 function compareTrue(n1, n2) {
   if (n1 === true && n2 === true) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 // Desafio 2
 function calcArea(base, altura) {
@@ -20,7 +19,7 @@ function splitSentence(a) {
 
 // Desafio 4
 function concatName(n) {
-  return n[n.length - 1] + ", " + n[0];
+  return `${n[n.length - 1]}, ${n[0]}`;
 }
 
 // Desafio 5
@@ -55,37 +54,46 @@ function catAndMouse(mouse, cat1, cat2) {
   let position1 = Math.abs(mouse - cat1);
   let position2 = Math.abs(mouse - cat2);
   if (position1 === position2) {
-    return "os gatos trombam e o rato foge"
-  } else if (position1 < position2) {
-    return 'cat1'
-  } else {
-    return 'cat2'
+    return 'os gatos trombam e o rato foge';
+  } if (position1 < position2) {
+    return 'cat1';
   }
+  return 'cat2';
 }
 console.log(catAndMouse(1, 0, 2));
 
 // Desafio 8
+// eslint-disable-next-line complexity
 function fizzBuzz(arrayNumbers) {
   let result = [];
-  for(let i = 0; i < arrayNumbers.length; i +=1){
-    if(arrayNumbers[i] % 3 === 0 && arrayNumbers[i] % 5 === 0){
-      result.push('fizzBuzz')
-    } else if (arrayNumbers[i] % 3 === 0){
-      result.push('fizz')
-    } else if (arrayNumbers[i] % 5 === 0){
-      result.push('buzz')
+  for (let i = 0; i < arrayNumbers.length; i += 1) {
+    if (arrayNumbers[i] % 3 === 0 && arrayNumbers[i] % 5 === 0) {
+      result.push('fizzBuzz');
+    } else if (arrayNumbers[i] % 3 === 0) {
+      result.push('fizz');
+    } else if (arrayNumbers[i] % 5 === 0) {
+      result.push('buzz');
     } else {
-      result.push('bug!')
+      result.push('bug!');
     }
-  }return result;
+  } return result;
 }
-console.log(fizzBuzz([2, 15, 7, 9, 45]))
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
+
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  const result = string.replaceAll('a', '1').replaceAll('e', '2').replaceAll('i', '3').replaceAll('o', '4').replaceAll('u', '5');
+
+  return result;
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  const result = string.replaceAll('1', 'a')
+.replaceAll('2', 'e')
+.replaceAll('3', 'i')
+.replaceAll('4', 'o')
+.replaceAll('5', 'u')
+  return result;
 }
 
 module.exports = {
